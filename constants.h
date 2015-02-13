adf35bb93008394a945a09b8c74269e4a15e047e
Thomas Perl <m@thp.io>
2015-02-13 11:59:07 +0100

    [misc] Update license headers from Git history

--- constants.h
+++ constants.h
@@ -1,4 +1,24 @@
 /**
+ * ssu: Seamless Software Update
+ * Copyright (c) 2012, 2013, 2014 Jolla Ltd.
+ * Contact: Bernd Wachter <bernd.wachter@jolla.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **/
+
+/**
  * @file constants.h
  * @copyright 2012 Jolla Ltd.
  * @author Bernd Wachter <bernd.wachter@jollamobile.com>
